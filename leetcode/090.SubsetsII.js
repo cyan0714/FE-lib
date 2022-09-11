6b6b267b51ef354044a0edc380c5b84b9db15fb6
Cyan <917244295@qq.com>
2022-09-11 09:17:11 +0800

    090.SubsetsII

--- leetcode/090.SubsetsII.js
+++ leetcode/090.SubsetsII.js
@@ -1,21 +1,23 @@
-// Given an integer array nums of unique elements, return all possible subsets (the power set).
+// Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
 
 // The solution set must not contain duplicate subsets. Return the solution in any order.
-//
+
 // Example 1:
+// Input: nums = [1,2,2]
+// Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
 
-// Input: nums = [1,2,3]
-// Output: [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
 // Example 2:
-
 // Input: nums = [0]
 // Output: [[],[0]]
 
-const subsets = nums => {
-  const result = []
+const subsetsWithDup = (nums) => {
+  const result = [];
+  nums.sort((a, b) => a - b)
+
   const backtrack = (start, curr) => {
     result.push([...curr])
     for (let i = start; i < nums.length; i++) {
+      if (i > start && nums[i] === nums[i - 1]) continue;
       curr.push(nums[i])
       backtrack(i + 1, curr)
       curr.pop()
