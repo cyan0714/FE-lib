976ef1a172166920e94ab97a3927906ab63624dc
Cyan <917244295@qq.com>
2022-09-21 07:46:48 +0800

    206

---
+++ leetcode/206.Reverse Linked List.js
@@ -0,0 +1,28 @@
+// Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+// Example 1:
+// Input: head = [1,2,3,4,5]
+// Output: [5,4,3,2,1]
+
+// Example 2:
+// Input: head = [1,2]
+// Output: [2,1]
+
+// Example 3:
+// Input: head = []
+// Output: []
+
+const reverseList = (head) => {
+  let prev = null;
+  let curr = head;
+  let next = null;
+
+  while (curr !== null) {
+    next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+
+  return prev
+}
