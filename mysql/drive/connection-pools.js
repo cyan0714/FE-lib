608624c6d48717dd611676cb8e5261b8f1d51e85
Cyan <917244295@qq.com>
2023-05-13 07:06:37 +0800

    connection pools

---
+++ mysql/drive/connection-pools.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql2');
+
+const connectionPool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  port: 3306,
+  password: '123456',
+  database: 'coderhub',
+  connectionLimit: 5,
+});
+
+// 预处理语句优点:
+// 1. 防止SQL注入
+// 2. 提高性能
+const statement = 'SELECT * FROM `user` WHERE name = ?'
+connectionPool.execute(statement, ['chenshiyan'], (err, values, fields) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(values);
+  console.log('fields: ', fields);
+})
